36a4668225bbb17821bf5e856bc553d2e207f675
Ryan Hawkins <rkhawkins80@hotmail.com>
2025-02-24 19:22:07 -0600

    Solved issue https://github.com/RyanKHawkins/Tic-Tac-Toe-JS/issues/1

--- src/script.js
+++ src/script.js
@@ -17,7 +17,44 @@ function switchPlayer() {
     currPlayer = currPlayer == player1 ? player2 : player1;
     messageDisplay.innerText = `Player:  ${currPlayer.symbol}`;
 }
-function gameWon() {}
+
+function gameWon() {
+    return isVerticalWin() || isHorizontalWin() || isDiagonalWin()
+}
+
+function isVerticalWin() {
+    const column1Cells = Array.from(document.querySelectorAll("[data-column = '1']"))
+    const column2Cells = Array.from(document.querySelectorAll("[data-column = '2']"))
+    const column3Cells = Array.from(document.querySelectorAll("[data-column = '3']"))
+    for (let column of [column1Cells, column2Cells, column3Cells]) {
+        if (column.every(cell => cell.classList.contains("placed") && cell.dataset.symbol == column[0].dataset.symbol)) {
+            winner = ""
+            return true
+        }
+    }
+    return false
+}
+
+function isHorizontalWin() {
+    const row1= Array.from(document.querySelectorAll("[data-row = '1']"))
+    const row2 = Array.from(document.querySelectorAll("[data-row = '2']"))
+    const row3 = Array.from(document.querySelectorAll("[data-row = '3']"))
+    for (let row of [row1, row2, row3]) {
+        if (row.every(cell => cell.classList.contains("placed") && cell.dataset.symbol == row[0].dataset.symbol)) {
+            winner = ""
+            return true
+        }
+    }
+}
+
+function isDiagonalWin() {
+    let diagonal1 = [document.getElementById("1"), document.getElementById("5"), document.getElementById("9")];
+    let diagonal2 = [document.getElementById("7"), document.getElementById("5"), document.getElementById("3")];
+    return (
+        diagonal1.every(cell => cell.classList.contains("placed") && cell.dataset.symbol == diagonal1[0].dataset.symbol)
+        || diagonal2.every(cell => cell.classList.contains("placed") && cell.dataset.symbol == diagonal2[0].dataset.symbol)
+    )
+}
 
 for (i = 0; i < cells.length; i++) {
     cells[i].addEventListener("click", (event) => {
@@ -62,18 +99,20 @@ function placeSymbol(cell) {
             startButton.classList.remove("alert-button");
         }, 500);
     }
-    switchPlayer();
     if (gameWon()) {
         winner = currPlayer;
         console.log(`${currPlayer.name} won!`);
         gameOver = true;
         messageDisplay.innerText = `${currPlayer.name} won!`;
+        return
     }
     if (!gameWon() && isBoardFull()) {
         gameOver = true;
         console.log("It was a draw");
         messageDisplay.innerText = "It's a draw";
+        return
     }
+    switchPlayer();
 }
 
 function isBoardFull() {
